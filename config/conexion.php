b1ef2510593cf02ff0130abeeb56e58105aa70bc
Moises-Pena <moises9798@gmail.com>
2024-12-07 13:32:47 -0400

    revamp files

---
+++ config/conexion.php
@@ -0,0 +1,9 @@
+
+<?php
+$mysqli = new mysqli('localhost', 'root', '', 'db_digiturno');
+if($mysqli->connect_error){
+	echo $error -> $mysqli->connect_error;
+}
+// usuario de ingreso: yordis
+// contraseña de ingreso:yordis  
+?>
